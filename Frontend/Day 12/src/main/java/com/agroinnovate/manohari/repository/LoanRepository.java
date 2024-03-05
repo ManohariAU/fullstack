4a22ac376fe203abc6c83f08794a89793f49d8e6
ManohariAU <117429123+ManohariAU@users.noreply.github.com>
2024-03-05 16:00:40 +0530

    Delete Frontend/Day 12 directory

--- Frontend/Day 12/src/main/java/com/agroinnovate/manohari/repository/LoanRepository.java
+++
@@ -1,9 +0,0 @@
-package com.agroinnovate.manohari.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.agroinnovate.manohari.model.Loan;
-
-public interface LoanRepository extends JpaRepository<Loan, String> {
-
-}
